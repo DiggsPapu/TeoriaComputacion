d542ac3125c5f6f82c515e09aa1a96e6789f203a
DiggsPapu <alo20172@uvg.edu.gt>
2023-11-16 18:33:09 -0600

    corrected the ex2 function

--- Ex2.hs
+++ Ex2.hs
@@ -1,14 +1,8 @@
 potenciaN :: Int -> [Int] -> [Int]
-potenciaN n = map (\x -> x ^ n)
+potenciaN result = map (^ result) 
 main :: IO ()
 main = do
     let listaEnteros = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let exponente = 10
-
-    putStrLn "Lista de enteros:"
-    print listaEnteros
-
-    let resultado = potenciaN exponente listaEnteros
-
-    putStrLn $ "\nPotencia " ++ show exponente ++ " de cada elemento:"
-    print resultado
+    let exponente = 3
+    let result = potenciaN exponente listaEnteros
+    print result
